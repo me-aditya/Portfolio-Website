95642eeb78d4ae4c4253813022438d4a54f82a45
me-aditya <adityarathi1212@gmail.com>
2022-08-22 04:20:05 +0530

    added education page and education details data

---
+++ client/src/Data/EducationDetails.js
@@ -0,0 +1,48 @@
+export const ResonanceEduventures = {
+    name: 'Resonance Eduventures',
+    type : 'JEE Coaching',
+    year : '2016 - 2018',
+    location : 'Nagpur',
+}
+
+export const VibrantAcademy = {
+    name: 'Vibrant Academy',
+    type : 'Drop Year',
+    year : '2018 - 2019',
+    location : 'Kota',
+}
+
+export const SchoolOfScholars = {
+    name: 'School Of Scholars',
+    type : 'Till class X',
+    year : '2005 - 2016',
+    location : 'Wardha',
+}
+
+export const StPaulJuniorCollege = {
+    name: 'St. Paul Junior College',
+    type : '+1 and +2 years',
+    year : '2016 - 2018',
+    location : 'Nagpur',
+}
+
+export const StAnthonyNationalSchool = {
+    name: 'St. Anthony National School',
+    type : 'Play School',
+    year : '2003 - 2005',
+    location : 'Wardha',
+}
+
+export const AgaragamiKindergarten = {
+    name: 'Agaragami Kindergarten',
+    type : 'LKG and UKG',
+    year : '2002 - 2003',
+    location : 'Wardha',
+}
+
+export const BITSPilani = {
+    name: 'BITS-Pilani',
+    type : 'Electrical and Electronics',
+    year : '2019 - 2023',
+    location : 'Goa',
+}
--- client/src/Pages/Education.jsx
+++ client/src/Pages/Education.jsx
@@ -1,8 +1,203 @@
 import React from 'react' ;
+import Timeline from '@mui/lab/Timeline';
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
+import TimelineDot from '@mui/lab/TimelineDot';
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
+import Typography from '@mui/material/Typography';
+import SchoolIcon from '@mui/icons-material/School';
+import FoundationIcon from '@mui/icons-material/Foundation';
+import ApartmentIcon from '@mui/icons-material/Apartment';
+import LocationCityIcon from '@mui/icons-material/LocationCity';
+import HomeWorkIcon from '@mui/icons-material/HomeWork';
+import MeetingRoomIcon from '@mui/icons-material/MeetingRoom';
+
+import { 
+  AgaragamiKindergarten, 
+  BITSPilani, 
+  ResonanceEduventures, 
+  SchoolOfScholars, 
+  StAnthonyNationalSchool, 
+  StPaulJuniorCollege, 
+  VibrantAcademy
+} from '../Data/EducationDetails' ;
 
 function Education() {
   return (
-    <div>Education</div>
+    <>
+      <Timeline position="alternate">
+        <TimelineItem>
+          <TimelineOppositeContent
+            sx={{ m: 'auto 0' }}
+            align="right"
+            variant="body2"
+            color="text.secondary"
+          >
+            {BITSPilani.year}
+          </TimelineOppositeContent>
+          <TimelineSeparator>
+            <TimelineConnector />
+            <TimelineDot color="secondary" sx={{ width: 50, height: 50 }}>
+                <SchoolIcon sx={{ width: 50, height: 50 }}/>
+            </TimelineDot>
+            <TimelineConnector sx={{ bgcolor: 'primary.main' }} />
+          </TimelineSeparator>
+          <TimelineContent sx={{ py: '12px', px: 2 }}>
+            <Typography variant="h5" component="span" color="secondary">
+              { BITSPilani.name }
+            </Typography>
+            <Typography variant="h6" color="secondary"> {BITSPilani.type} </Typography>
+            <Typography variant="subtitle1" color="secondary"> {BITSPilani.location} </Typography>
+          </TimelineContent>
+        </TimelineItem>
+        <TimelineItem>
+          <TimelineOppositeContent
+            sx={{ m: 'auto 0' }}
+            align="right"
+            variant="body2"
+            color="text.secondary"
+          >
+            {VibrantAcademy.year}
+          </TimelineOppositeContent>
+          <TimelineSeparator>
+            <TimelineConnector sx={{ bgcolor: 'primary.main' }} />
+            <TimelineDot color="primary" sx={{ width: 50, height: 50 }}>
+                <ApartmentIcon sx={{ width: 50, height: 50 }}/>
+            </TimelineDot>
+            <TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
+          </TimelineSeparator>
+          <TimelineContent sx={{ py: '12px', px: 2 }}>
+            <Typography variant="h5" component="span" color="primary">
+              { VibrantAcademy.name}
+            </Typography>
+            <Typography variant="h6" color="primary">{VibrantAcademy.type}</Typography>
+            <Typography variant="subtitle1" color="primary"> {VibrantAcademy.location} </Typography>
+          </TimelineContent>
+        </TimelineItem>
+        <TimelineItem>
+        <TimelineOppositeContent
+          sx={{ m: 'auto 0' }}
+          align="right"
+          variant="body2"
+          color="text.secondary"
+        >
+          {ResonanceEduventures.year}
+        </TimelineOppositeContent>
+        <TimelineSeparator>
+          <TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
+          <TimelineDot color="secondary" sx={{ width: 50, height: 50 }}>
+                <HomeWorkIcon sx={{ width: 50, height: 50 }}/>
+          </TimelineDot>
+          <TimelineConnector sx={{ bgcolor: 'primary.main' }} />
+        </TimelineSeparator>
+        <TimelineContent sx={{ py: '12px', px: 2 }} >
+          <Typography variant="h5" component="span" color="secondary">
+            {ResonanceEduventures.name}
+          </Typography>
+          <Typography variant="h6" color="secondary">{ResonanceEduventures.type}</Typography>
+          <Typography variant="subtitle1" color="secondary"> {ResonanceEduventures.location} </Typography>
+        </TimelineContent>
+      </TimelineItem>
+        <TimelineItem>
+          <TimelineOppositeContent
+            sx={{ m: 'auto 0' }}
+            align="right"
+            variant="body2"
+            color="text.secondary"
+          >
+            {StPaulJuniorCollege.year}
+          </TimelineOppositeContent>
+          <TimelineSeparator>
+            <TimelineConnector sx={{ bgcolor: 'primary.main' }} />
+            <TimelineDot color="primary" sx={{ width: 50, height: 50 }}>
+                <LocationCityIcon sx={{ width: 50, height: 50 }}/>
+            </TimelineDot>
+            <TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
+          </TimelineSeparator>
+          <TimelineContent sx={{ py: '12px', px: 2 }}>
+            <Typography variant="h5" component="span" color = 'primary'>
+              {StPaulJuniorCollege.name}
+            </Typography>
+            <Typography variant="h6" color = 'primary'>{StPaulJuniorCollege.type}</Typography>
+            <Typography variant="subtitle1" color = 'primary'> {StPaulJuniorCollege.location} </Typography>
+          </TimelineContent>
+        </TimelineItem>
+        <TimelineItem>
+          <TimelineOppositeContent
+            sx={{ m: 'auto 0' }}
+            variant="body2"
+            color="text.secondary"
+          >
+            {SchoolOfScholars.year}
+          </TimelineOppositeContent>
+          <TimelineSeparator>
+            <TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
+            <TimelineDot color = "secondary" sx={{ width: 50, height: 50 }} >
+                <AccountBalanceIcon sx={{ width: 50, height: 50 }}/>
+            </TimelineDot>
+            <TimelineConnector sx={{ bgcolor: 'primary.main' }} />
+          </TimelineSeparator>
+          <TimelineContent sx={{ py: '12px', px: 2 }}>
+            <Typography variant="h5" component="span" color = 'secondary'>
+              {SchoolOfScholars.name}
+            </Typography>
+            <Typography variant="h6" color = 'secondary'> {SchoolOfScholars.type}</Typography>
+            <Typography variant="subtitle1" color = 'secondary'> {SchoolOfScholars.location} </Typography>
+          </TimelineContent>
+        </TimelineItem>
+        <TimelineItem>
+        <TimelineOppositeContent
+            sx={{ m: 'auto 0' }}
+            align="right"
+            variant="body2"
+            color="text.secondary"
+          >
+            {AgaragamiKindergarten.year}
+          </TimelineOppositeContent>
+          <TimelineSeparator>
+            <TimelineConnector sx={{ bgcolor: 'primary.main' }} />
+            <TimelineDot color="primary" sx={{ width: 50, height: 50 }}>
+                <MeetingRoomIcon sx={{ width: 50, height: 50 }}/>
+            </TimelineDot>
+            <TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
+          </TimelineSeparator>
+          <TimelineContent sx={{ py: '12px', px: 2 }}>
+            <Typography variant="h5" component="span" color = 'primary.main'>
+              {AgaragamiKindergarten.name}
+            </Typography>
+            <Typography variant="h6" color = 'primary.main'>{AgaragamiKindergarten.type}</Typography>
+            <Typography variant="subtitle1" color = 'primary.main'> {AgaragamiKindergarten.location} </Typography>
+          </TimelineContent>
+        </TimelineItem>
+        <TimelineItem>
+          <TimelineOppositeContent
+              sx={{ m: 'auto 0' }}
+              align="right"
+              variant="body2"
+              color="text.secondary"
+          >
+            {StAnthonyNationalSchool.year}
+          </TimelineOppositeContent>
+          <TimelineSeparator>
+            <TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
+              <TimelineDot color="secondary" sx={{ width: 50, height: 50 }}>
+                  <FoundationIcon sx={{ width: 50, height: 50 }}/>
+              </TimelineDot>
+            <TimelineConnector />
+          </TimelineSeparator>
+          <TimelineContent sx={{ py: '12px', px: 2 }} color = 'secondary.main'>
+            <Typography variant="h5" component="span">
+              {StAnthonyNationalSchool.name}
+            </Typography>
+            <Typography variant="h6" color = 'secondary.main'>{StAnthonyNationalSchool.type}</Typography>
+            <Typography variant="subtitle1"> {StAnthonyNationalSchool.location} </Typography>
+          </TimelineContent>
+        </TimelineItem>
+      </Timeline>
+    </>
   )
 }
 
