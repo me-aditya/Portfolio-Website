8ee47a8c85240d57a66d0850f4fba1257f8f7d71
Aaditya Rathi <adityarathi1212@gmail.com>
2022-08-24 04:47:46 +0530

    added experience and its details

--- client/src/Pages/Experience.jsx
+++ client/src/Pages/Experience.jsx
@@ -1,8 +1,70 @@
 import React from 'react' ;
+import Timeline from '@mui/lab/Timeline';
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineDot from '@mui/lab/TimelineDot';
+import { Grid, Typography, Container, useTheme, useMediaQuery } from '@mui/material';
+import { AMAZON, ELECTRONO } from './ExperienceDetails';
 
-function Experience() {
+const Experience = () => {
+
+  const theme = useTheme() ;
+  const mobile = useMediaQuery(theme.breakpoints.down('md')) ;
+  const heading = mobile ? 'h6' : 'h3' ; 
+  const title = mobile ? 'inherit' : 'h4' ;
+  const description = mobile ? 'subtitle2' : 'h6' ;
+
   return (
-    <div>Experience</div>
+    <>
+      <Grid container
+          spacing={0}
+          direction= 'row'
+          alignItems="center"
+          justifyContent="center"
+      >
+        <Timeline position="alternate">
+          <TimelineItem>
+            <TimelineSeparator>
+              <TimelineDot color="secondary" />
+              <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent>
+              <Grid>
+                <Grid item>
+                <Container>
+                    <Typography variant = {heading} color= '#E91E63'> {AMAZON.name} </Typography>
+                    <Typography variant = {title} color = '#FF4081'>{AMAZON.title}</Typography>
+                    <Typography variant = {description} color= 'secondary.light'>{AMAZON.description1}</Typography>
+                    <Typography variant = {description} color= 'secondary.light'>{AMAZON.description2}</Typography>
+                    <Typography variant = {description} color= 'secondary.light'>{AMAZON.description3}</Typography>
+                  </Container>
+                </Grid>
+              </Grid>
+            </TimelineContent>
+          </TimelineItem>
+          <TimelineItem>
+            <TimelineSeparator>
+              <TimelineDot color="success" />
+            </TimelineSeparator>
+            <TimelineContent>
+              <Grid>
+                <Grid item >
+                  <Container>
+                    <Typography variant = {heading} color= '#388E3C'>{ELECTRONO.name}</Typography>
+                    <Typography variant = {title} color='#7CB342'>{ELECTRONO.title}</Typography>
+                    <Typography variant = {description} color= '#00897B'>{ELECTRONO.description1}</Typography>
+                    <Typography variant = {description} color= '#00897B'>{ELECTRONO.description2}</Typography>
+                  </Container>
+                </Grid>
+              </Grid>
+            </TimelineContent>
+          </TimelineItem>
+        </Timeline>
+      </Grid>
+      
+    </>
   )
 }
 
---
+++ client/src/Pages/ExperienceDetails.js
@@ -0,0 +1,14 @@
+export const AMAZON = {
+    name : 'Amazon [PPO Offered]',
+    title : 'SDE Intern',
+    description1 : 'Added a new feature of on-boarding a metric.',
+    description2 : 'Made front-end using React and Meridian UI.',
+    description3 : 'Implemented backend using Coral Framework and Google Guice.',
+};
+
+export const ELECTRONO = {
+    name : 'Electrono Solutions',
+    title : 'Backend Engineer',
+    description1 : 'Migrated existing mongoDB database to elastic search.',
+    description2 : 'Made dashboard for machine data analysis using Kibana.',
+}
